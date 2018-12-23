361d71f13de326e563b113fef09cb6245bbe8111
rishishukla042 <rishishukla042@gmail.com>
2018-12-23 13:40:06 +0530

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+   recipes: Recipe[] = [
+     new Recipe('A test Recipe','This is simply a test','https://c.pxhere.com/photos/b6/a1/esfiha_kibe_tidbits_snacks_power_supply-1375826.jpg!d')
+   ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
